b9ed2dbc842a420e4483e6465cbc63d95b4665ea
Thomas Bouchard <thomaswardbouchard@gmail.com>
2019-06-13 22:37:31 +0200

    ADD: api endpoint to get all movies

--- controllers/movies.js
+++ controllers/movies.js
@@ -2,6 +2,15 @@
 const MovieSchema = require('../models/Movie.js');
 
 module.exports.controller = (app) => {
+  app.get('/movies', (req, res) => {
+    MovieSchema.find({}, 'name description genre release_year', (error, movies) => {
+      if (error) { console.log(error); }
+      res.send({
+        movies,
+      });
+    });
+  });
+
   app.post('/movies', (req, res) => {
     const newMovie = new MovieSchema({
       name: req.body.name,
